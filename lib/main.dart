1b8927b59b57ae480c2c97d063fdcbe47d383d3a
Edward <Edward Bryan Abergas>
2022-12-09 16:50:49 +0800

    Updated maps and string resource

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pet Monitor',
       theme: ThemeData(
         // This is the theme of your application.
         //
